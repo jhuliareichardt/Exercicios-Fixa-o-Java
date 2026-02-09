bee07bb3ee872b42c754211ade50b39830bdd0a0
Jhulia Reichardt <jhulia_reichardt@estudante.sesisenai.org.br>
2026-02-09 09:41:29 -0300

    Atividade Fixação.add

---
+++ AT_03/exercicio-1/Exercicio1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; // Importa a classe Scanner
+public class Exercicio1 {  // Declara a classe chamada Exercicio1
+ public static void main(String[] args) { //aqui o sistema começa a executar
+ System.out.println("==========================");  //estética
+  Scanner scanner = new Scanner(System.in); //ativa scanner
+
+    System.out.print("Digite o nome do sensor: ");  //Pede nome do sensor
+    String nomeSensor = scanner.nextLine();  //armazena o nome do sensor
+    
+    System.out.print("Digite o valor: "); //Pede nome do sensor
+    int valorSensor = scanner.nextInt();  //armazena o valor do sensor
+    
+    System.out.println("Sensor " + nomeSensor + " registrado com o valor de " + valorSensor + " unidades.");
+        // Exibe os dados na tela
+scanner.close();  //fecha scanner
+ }
+}
